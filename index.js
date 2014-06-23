187068fbff38d5e4c4172c7d67313393f24863af
gaspard <gaspard@gmail.com>
2014-06-23 17:52:29 +0200

    correcting Array.splice()

--- index.js
+++ index.js
@@ -57,13 +57,10 @@ var clean = function (rawfile, fn) {
 
 var dowatch = function() {
 	var watcher = new emitter();
-	console.log("watching with db of %d elements", fl.db.length);
+
 	watch(config.directory, config.options, filter(config.pattern, function(filename) {
 
-		console.log('watch: %s changed, %d in db.',filename, fl.db.length);
-
 		if(fs.existsSync(filename)) {
-			console.log("is file %b",fs.statSync(filename).isFile());
 			watcher.emit("created", filename);
 			
 			// remove any duplicate (sometimes watch does not properly trigger)
@@ -79,20 +76,17 @@ var dowatch = function() {
 		}
 		else {
 			console.log("fl: deleted or moved ? this should be solved with a setTimeout array attached to the object");
-			watcher.emit("deleted", filename);
 			// remove from db
 			findOne(fl.db, path.normalize(filename), function(i) {
-				var deleted = fl.db.splice(fl.db, i ,1);
-				console.log("deleted: %o", deleted);
+				var deleted = fl.db.splice(i ,1);
+				console.log("fl: deleted: %d", i, deleted);
 			});
 
 		}
 	}));
 
 	findOne = function(db,filename, cb) {
-		console.log("finding filename %s from %d elements", filename, db.length);
 		for (var i = 0; i < db.length; i++) {
-			console.log("findOne:",db[i].fullpath,filename);
 			if(db[i].fullpath === filename)
 				cb(i);
 		}
@@ -105,11 +99,4 @@ var dowatch = function() {
 fl.db= populate();
 fl.watch= dowatch();
 
-
-
-// module.exports = fl;
-
-console.log("global fl.db contains %d elements",fl.db.length);
-
-fl.watch.on('created', function(f){ console.log("created, %s db contains %d", f, fl.db.length);});
-fl.watch.on('deleted', function(f){ console.log("deleted, %s db contains %d", f, fl.db.length);})
+module.exports = fl;
