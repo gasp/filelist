e3aa39fb5e6fefed11d1b538d5a777b3e5ccee96
gaspard <gaspard@gmail.com>
2014-07-16 11:11:49 +0200

    filters: added simple filters

--- index.js
+++ index.js
@@ -5,124 +5,62 @@ var fs = require('fs'),
 	events = require('events'),
 	_ = require('lodash');
 
-var defaultConfig = {
+var defaults = {
 	directory: path.join(__dirname, 'somedir'),
 	options: { recursive: false, followSymLinks: false }, 
-	pattern: /\.(ogg|mp3)$/,
+	filter: /\.(ogg|mp3)$/,
 	events: ['created', 'deleted', 'updated'],
-	watch: true
+	recursive: false,
+	watch: false,
+	async: false
 };
 
 
-var emitter = new events.EventEmitter();
 var list = function(config) {
-	_.extend(config, defaultConfig);
+	options = _.defaults(config, defaults);
 
-	var populate = function() {
-		var dirfiles = fs.readdirSync(config.directory),
-			cleanfiles = [];
-		for (var i = 0; i < dirfiles.length; i++) {
-			clean(dirfiles[i], function (file) {
-				cleanfiles.push(file);
-			});
-		}
-		return cleanfiles;
-	};
+	var that = this;
+	var List = (function(options) {
 
-	var filter = function(pattern, fn) {
-		return function(filename) {
-			if (pattern.test(filename)) {
+		var listSync = function(options) {
+			var dirfiles = fs.readdirSync(options.directory),
+				files = [];
+			for (var i = 0; i < dirfiles.length; i++) {
+				filter(options.filter, function(filename) {
+					console.log(filename);
+					files.push({name: filename});
+				})(dirfiles[i]);
+			}
+			return files;
+		};
+
+		var filter = function(pattern, fn) {
+			if(!pattern) return function(filename) {
 				fn(filename);
+			};
+			else return function(filename) {
+				if (pattern.test(filename)) {
+					fn(filename);
+				}
 			}
+		};
+
+		if(!options.watch && !options.async && !options.recursive) {
+			return {
+				db: listSync(options)
+			};
 		}
-	};
-
-	var clean = function (rawfile, fn) {
-		console.log("cleaning %s", rawfile);
-		filter(config.pattern, function(filename) {
-			var file = (function(config,filename) {
-				if(filename[0] === path.sep)
-					return {
-						fullpath: path.normalize(filename),
-						name: path.basename(filename)
-					};
-				else
-					return {
-						fullpath: path.normalize(path.join(config.directory, filename)),
-						name: filename
-					};
-			})(config,filename);
-			if(fs.statSync(file.fullpath).isFile()) {
-				file.stats = fs.statSync(file.fullpath);
-				fn(file);
-			}
-		})(rawfile);
-	};
-
-	var findOne = function(db, filename, cb) {
-		for (var i = 0; i < db.length; i++) {
-			if(db[i].fullpath === filename)
-				cb(i);
+		else {
+			var emitter = new events.EventEmitter(),
+				ret = {
+					db: []
+				};
+			util.inherits(emitter, ret);
+			return emitter;
 		}
-	};
+	})(options);
 
-	var dowatch = function() {
-
-		watch(config.directory, config.options, filter(config.pattern, function(filename) {
-
-			if(fs.existsSync(filename)) {
-				emitter.emit("created", filename);
-			
-				// remove any duplicate (sometimes watch does not properly trigger)
-				findOne(list.db, path.normalize(filename), function(i) {
-					list.db.splice(fl.db, i ,1); // fixme cannot access this !
-				});
-			
-				// add to db
-				clean(filename, function(file) {
-					list.db.push(file); // fixme cannot access this !
-				})
-			
-			}
-			else {
-				console.log("fl: deleted or moved ? this should be solved with a setTimeout array attached to the object");
-				// remove from db
-				 // fixme cannot access this fl.db !
-				findOne(list.db, path.normalize(filename), function(i) {
-					var deleted = list.db.splice(i ,1);
-					console.log("fl: deleted: %d", i, deleted);
-				});
-			}
-		}));
-	};
-
-	if(config.watch) {
-		dowatch();
-	};
-
-	return {
-		db: populate()
-	}
+	return List;
 };
 
-util.inherits(emitter, list);
-
-
-// debug shit
-
-var filelist = list(defaultConfig);
-console.log(filelist);
-
-setTimeout(function () {
-	console.log("filelist.db is...");
-	for (var i = 0; i < filelist.db.length; i++) {
-		console.log(filelist.db[i].name);
-	}
-},5000);
-
-emitter.on("created", function (filename) {
-	console.log("created", filename);
-});
-
-/*
-module.exports = fl;*/
+module.exports = list;
--- tests/listing.js
+++ tests/listing.js
@@ -2,24 +2,61 @@ var filelist = require("../index.js"),
 	fs = require("fs"),
 	path = require("path");
 
+var directory = path.join(__dirname, 'data'),
+	shortdirectory = (function(maxlength){
+		if(directory.length < maxlength)
+			return directory;
+		else {
+			var begining = directory.substring(
+				0, Math.floor(maxlength / 2) - 1);
+			var end = directory.substring(
+				Math.max(directory.length - (Math.floor(maxlength / 2) - 2), 0),
+				directory.length);
+
+			return begining + '...' + end;
+		}
+	})(40)
 
 describe("simply listing files in directory", function() {
 
 	var options = {
-		directory: path.join(__dirname, 'data'),
+		directory: directory,
 		recursive: false,
 		filter: null,
 		async: false,
 		watch: false
 	};
 
-	it("lists synchronously the folder" +  options.directory, function() {
+	it("lists synchronously the folder" + shortdirectory, function() {
 		var fl = filelist(options);
 
 		expect(fl.db.length).toBeGreaterThan(2);
+		expect(fl.db.length).toBe(7);
 		expect(fl.db[0]).toBeDefined();
 		expect(fl.db[0].name).toBeDefined();
 		expect(fl.db[0].name).toBe('file-0.txt');
 	});
 
 });
+
+
+describe("listing files in directory with options", function() {
+
+	var options = {
+		directory: path.join(__dirname, 'data'),
+		recursive: false,
+		filter: /\.txt$/,
+		async: false,
+		watch: false
+	};
+
+	it("lists only .txt files in" +  options.directory, function() {
+		var fl = filelist(options);
+		expect(fl.db.length).toBe(6);
+		for (var i = 0; i < fl.db.length; i++) {
+			expect(fl.db[i].name).toBeDefined();
+			expect(fl.db[i].name.substring(fl.db[i].name.length-4)).toBe('.txt');
+		}
+	});
+});
+
